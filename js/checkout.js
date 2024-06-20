5f81ffbb25aef9fe91c53fccdccc21b4ee7d0cfc
lelongvtvp <lelong122004@gmail.com>
2024-06-21 02:29:21 +0700

    button xóa sản phẩm

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../js/cart.js";
+import { cart, removeFromCart } from "../js/cart.js";
 import { products } from "../data/products.js";
 let cartSummaryHTML = '';
 cart.forEach ((CartItem) => {
@@ -35,7 +35,7 @@ cart.forEach ((CartItem) => {
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -89,3 +89,10 @@ cart.forEach ((CartItem) => {
 })
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
+document.querySelectorAll('.js-delete-link')
+    .forEach((link) => {
+        link.addEventListener('click', () => {
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+        })
+    })
