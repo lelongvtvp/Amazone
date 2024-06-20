6c17c1c17d4a5f5cdcdd53030ef85f322f734faa
lelongvtvp <lelong122004@gmail.com>
2024-06-21 02:35:01 +0700

    ấn vào nút xóa trên màn hình

--- js/checkout.js
+++ js/checkout.js
@@ -12,7 +12,7 @@ cart.forEach ((CartItem) => {
     });
     cartSummaryHTML += 
     `
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Wednesday, June 15
         </div>
@@ -94,5 +94,7 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         })
     })
